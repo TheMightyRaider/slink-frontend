67a41963085916414d74b7fb7f4a565ed6c3db7d
TheMightyRaider <sivasanjay7@gmail.com>
2020-04-13 19:53:18 +0530

    Fetched shortenURL for inputURL

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import DisplayShortenedLink from "./Components/DisplayShortenedLink";
 import DisplayStoredLinks from "./Components/DisplayStoredLinks";
 import "./app.css";
@@ -7,9 +8,22 @@ class App extends React.Component {
   state = {
     value: "",
     receivedShortedLink: false,
+    displayStoredShortenedLink: false,
     shortedLink: "",
     storedLinks: [],
   };
+
+  componentDidMount() {
+    let userID = parseInt(localStorage.getItem("ID"));
+    if (userID) {
+      this.setState({
+        userID,
+      });
+    } else {
+      let newUserId = new Date().getTime();
+      localStorage.setItem("ID", newUserId);
+    }
+  }
 
   updateValue = (e) => {
     this.setState({
@@ -17,15 +31,27 @@ class App extends React.Component {
     });
   };
 
-  submitTheLink = (e) => {
+  submitTheLink = async (e) => {
+    const params = {
+      url: this.state.value,
+      user: 12345,
+    };
+
+    const shortenLink = await axios.put(
+      "https://cors-anywhere.herokuapp.com/http://slink-staging.herokuapp.com/api/links",
+      params
+    );
+
     this.setState({
-      shortedLink: "gotit",
       receivedShortedLink: true,
+      displayStoredShortenedLink: false,
+      shortedLink: shortenLink.data.data.slink,
     });
   };
 
   fetchStoredLinks = () => {
     this.setState({
+      displayStoredShortenedLink: true,
       storedLinks: [
         1,
         2,
@@ -74,7 +100,8 @@ class App extends React.Component {
           />
         ) : null}
 
-        {this.state.storedLinks.length > 0 ? (
+        {this.state.storedLinks.length > 0 &&
+        this.state.displayStoredShortenedLink ? (
           <DisplayStoredLinks links={this.state.storedLinks} />
         ) : null}
       </div>
